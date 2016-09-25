4f836f8ee62e73ccd488dd693fb916e9a87fcecf
pearlscream <dimonich41@gmail.com>
2016-09-25 13:20:30 +0300

    Pagination corrected

--- src/main/java/com/budko/elibrary/controllers/HelloController.java
+++ src/main/java/com/budko/elibrary/controllers/HelloController.java
@@ -6,6 +6,7 @@ import com.budko.elibrary.services.BookService;
 import com.budko.elibrary.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
--- src/main/java/com/budko/elibrary/services/impl/BookServiceImpl.java
+++ src/main/java/com/budko/elibrary/services/impl/BookServiceImpl.java
@@ -5,6 +5,7 @@ import com.budko.elibrary.repositories.BookRepository;
 import com.budko.elibrary.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +27,7 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Page<Book> getAllBooks(Pageable pageable) {
-        return bookRepository.findAll(pageable);
+        Pageable request = new PageRequest(pageable.getPageNumber()-1,pageable.getPageSize(),pageable.getSort());
+        return bookRepository.findAll(request);
     }
 }
